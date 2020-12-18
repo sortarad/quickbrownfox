fc06795eebdb17cdfeec82541f4e75f491c31032
George Mamadashvili <georgemamadashvili@gmail.com>
2020-12-18 13:39:48 +0400

    Scaffold Tag

---
+++ src/Tags/QuickBrownFoxTags.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sortarad\QuickBrownFox\Tags;
+
+use Statamic\Tags\Tags;
+
+class QuickBrownFoxTags extends Tags
+{
+    protected static $handle = 'quick_brown_fox';
+
+    /**
+     * The {{ quick_brown_fox }} tag.
+     *
+     * @return string|array
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * The {{ quick_brown_fox:example }} tag.
+     *
+     * @return string|array
+     */
+    public function example()
+    {
+        //
+    }
+}
