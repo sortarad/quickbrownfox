79631413e76bef21503747b41bef537b02fada30
George Mamadashvili <georgemamadashvili@gmail.com>
2020-12-20 20:37:08 +0400

    Fixes assets

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Sortarad\QuickBrownFox;
 
-use Sortarad\QuickBrownFox\Tags\QuickBrownFoxTags;
+use Statamic\Statamic;
+use Statamic\Support\Str;
+use Statamic\Facades\Addon;
 use Statamic\Providers\AddonServiceProvider;
+use Sortarad\QuickBrownFox\Tags\QuickBrownFoxTags;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -11,5 +14,40 @@ class ServiceProvider extends AddonServiceProvider
         QuickBrownFoxTags::class,
     ];
 
+    protected $publishables = [
+        __DIR__.'/../public' => '',
+    ];
+
     protected $viewNamespace = 'qbf';
+
+    public function boot()
+    {
+        parent::boot();
+
+        $this->bootAddonPublishAfterInstall();
+    }
+
+    public function bootAddonPublishAfterInstall()
+    {
+        if (! $this->publishAfterInstall) {
+            return $this;
+        }
+
+        if (empty($this->publishables)) {
+            return $this;
+        }
+
+        $addon = Addon::all()->first(function ($addon) {
+            return Str::startsWith(get_class($this), $addon->namespace());
+        });
+
+        Statamic::afterInstalled(function ($command) use ($addon) {
+            $command->call('vendor:publish', [
+                '--tag' => $addon->slug(),
+                '--force' => true,
+            ]);
+        });
+
+        return $this;
+    }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.disableNotifications();
-
+mix.setPublicPath('public');
 mix
   .sass('resources/sass/quick-brown-fox.scss', 'public/css')
   .options({
